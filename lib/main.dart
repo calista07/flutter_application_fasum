9390a3f7f9eefbd13df2e78055b3c084608717ed
calista07 <viachristian282@gmail.com>
2026-05-05 20:30:08 +0700

    asdskjhf

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_fasum/firebase_options.dart';
 import 'package:flutter_application_fasum/screens/sign_in_screen.dart';
 import 'package:flutter_application_fasum/screens/sign_up_screen.dart';
+import 'package:flutter_application_fasum/screens/Home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
@@ -14,16 +16,36 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Fasum App',
+
+      debugShowMaterialGrid: false,
+
       theme: ThemeData(
+        useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+
+        appBarTheme: const AppBarTheme(centerTitle: true),
+
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(),
+        ),
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: const SignInScreen(),
+
+      initialRoute: '/signin',
+
+      routes: {
+        '/signin': (context) => const SignInScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        '/home': (context) => const HomeScreen(),
+      },
+
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (context) => const SignUpScreen());
+      },
     );
   }
 }
--- lib/screens/Home_screen.dart
+++ lib/screens/Home_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_fasum/screens/add_post_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,7 +17,63 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Screen')),
-      body: const Center(child: Text('Welcome to the Home Screen!')),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('Belum ada data'));
+          }
+
+          final posts = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: posts.length,
+            itemBuilder: (context, index) {
+              final data = posts[index].data() as Map<String, dynamic>;
+
+              // decode base64 ke bytes
+              Uint8List imageBytes = base64Decode(data['image']);
+
+              return Card(
+                margin: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.memory(imageBytes),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        data['description'] ?? '',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Text('By: ${data['fullName']}'),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const AddPostScreen(),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      
     );
   }
 }
